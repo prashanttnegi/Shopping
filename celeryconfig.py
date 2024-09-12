e4e8009add5034192c3d511b9cb73631e3c9781f
prashanttnegi <144382585+prashanttnegi@users.noreply.github.com>
2024-09-12 22:17:28 +0530

    Update celeryconfig.py

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,4 +1,4 @@
-broker_url = "redis://localhost:6379/1"
-result_backend = "redis://localhost:6379/2"
+broker_url = "redis://red-crhh9m56l47c73c90ih0:6379/1"
+result_backend = "redis://red-crhh9m56l47c73c90ih0:6379/2"
 timezone = "Asia/kolkata"
 broker_connection_retry_on_startup=True
